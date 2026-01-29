67230b3e9f8472d30f7f0e76c540e6d728c18bf2
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-01-29 15:09:08 +0500

    Backend Port updated

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use((req, res) => {
 });
 
 //Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
