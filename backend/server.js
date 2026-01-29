661cda4bb89e732817f004b222d7362065f103bc
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-01-29 13:14:10 +0500

    Backend server file

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import connectDB from "./config/db.js";
+import errorHandler from "./middleware/errorHandler.js";
+
+import authRoutes from "./routes/authRoutes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json()); //JSON body parser
+app.use(express.urlencoded({ extended: false })); //Form data parser
+
+//static folder for uploads
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//Routes
+app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
+
+//404 handler
+app.use((req, res) => {
+  res.status(404).json({ success: false, error: "Route not found" });
+});
+
+//Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error(`Error: ${err.message}`);
+  process.exit(1);
+});
