bde06850df4b4f0a8e37ceac3fcca254d022bada
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-02-05 14:33:57 +0500

    Backend db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
