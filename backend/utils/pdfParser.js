15a56956d1be67d4d723b194590a033add726a58
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-03-04 00:59:45 +0500

    extractTextFromPDF

---
+++ backend/utils/pdfParser.js
@@ -0,0 +1,7 @@
+/**
+ *Extract text from PDF file
+ * @param {string} filePath - Path to the PDF file
+ * @returns {Promise<{text: string; numages: number}>} - Promise that resolves to the extracted text
+ */
+
+export const extractTextFromPDF = async (filePath) => {};
